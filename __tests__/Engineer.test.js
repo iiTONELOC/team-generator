567c36a42cd5f6738936446264cc97f7387ebc11
iiTONELOC <anthonytropeano@me.com>
2021-02-20 20:07:20 -0500

    added engineer and tests, finally fixed the retRole() test.

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,11 @@
+const Engineer = require('../lib/Engineer');
+
+test('creates an engineer object', () => {
+    const engineer = new Engineer ('Dave', '4', 'test@email.com', 'githubUserName');
+
+    expect(engineer.name).toBe('Dave');
+    expect(engineer.id).toBe('4');
+    expect(engineer.email).toBe('test@email.com');
+    expect(engineer.github).toBe('githubUserName');
+    expect(engineer.getRole()).toBe('Engineer');
+})
