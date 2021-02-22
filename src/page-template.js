461cbffb76beb78bbc9eec36eede316ccd83ac0f
iiTONELOC <anthonytropeano@me.com>
2021-02-22 14:25:28 -0500

    got manager engineer and intern to populate

--- src/page-template.js
+++ src/page-template.js
@@ -1,4 +1,4 @@
-const renderEngineer = require('../utils/renderFunctions');
+
 
 class GeneratePage {
     constructor(
@@ -41,65 +41,67 @@ class GeneratePage {
 
     createPage() {
         // console.log(this.manager)
-        console.log([this.data.manager])
+        console.log(this.data)
+        console.log(this.data[0].getRole())
         // console.log("You might have it fuck face!")
         //console.log(this.manager.getRole());
 
-    //     return `
-    // <!doctype html>
-    // <html lang="en">
-
-    // <head>
-    //     <meta charset="utf-8" />
-    //     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    //     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    //     <meta name="description" content="Dynamic team profile generator">
-    //     <meta name="author" content="Anthony Tropeano">
-    //     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
-    //         integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-    //     <link rel="stylesheet" href="./style.css">
-    //     <script src="https://kit.fontawesome.com/c7e6266a9c.js" crossorigin="anonymous"></script>
-    //     <title>Profile Generator</title>
-    // </head>
-
-    // <body>
-    //     <main>
-    //         <div class="container-fluid">
-    //             <div class="row">
-    //                 <div class="col-sm-12 bg-dark text-center">
-    //                     <div class="title d-flex flex-row justify-content-center align-items-center">
-    //                         <h1 class="text-warning">My Team</h1>
-    //                     </div>
-    //                 </div>
-    //             </div>
-    //             <div id="cards" class="container mt-5">
-    //                 <div class="row d-flex justify-content-center align-items-center gy-4">
-    //                 <div class="col-1" style="width: 18rem;">
-    //                     <div class="card bg-dark text-dark">
-    //                         <div class="card-body bg-warning">
-    //                             <h2>${this.data.manager.getName()}</h2>
-    //                             <p class="h4"><span><i class="fas fa-user-tie"></i> </span>${this.data.manager.getRole()}</p>
-    //                         </div>
-    //                         <div class="card-body text-white">
-    //                             <ul class="list-group list-group-flush my-4 ">
-    //                                 <li class="list-group-item bg-secondary">ID: ${this.data.manager.getID()} </li>
-    //                                 <li class="list-group-item bg-secondary"><a href="mailto:${this.data.manager.getEmail()}"> Email:
-    //                                 ${this.data.manager.getEmail()} </a> </li>
-    //                                 <li class="list-group-item bg-secondary">Office number: ${this.data.manager.getOffice()}</li>
-    //                             </ul>
-    //                         </div>
-    //                         ${renderEngineer(this.data.engineer)}
-    //                     </div>
-    //                 </div>
-    //                 </div>
-    //             </div>
-    //         </div>
-    //     </main>
-    // </body>
-
-    // </html>
-    // `
-
+        return `
+        <!doctype html>
+        <html lang="en">
+        
+        <head>
+            <meta charset="utf-8" />
+            <meta http-equiv="x-ua-compatible" content="ie=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta name="description" content="Dynamic team profile generator">
+            <meta name="author" content="Anthony Tropeano">
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
+                integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+            <link rel="stylesheet" href="./style.css">
+            <script src="https://kit.fontawesome.com/c7e6266a9c.js" crossorigin="anonymous"></script>
+            <title>Profile Generator</title>
+        </head>
+        
+        <body>
+            <main>
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-sm-12 bg-dark text-center">
+                            <div class="title d-flex flex-row justify-content-center align-items-center">
+                                <h1 class="text-warning">My Team</h1>
+                            </div>
+                        </div>
+                    </div>
+                    <div id="cards" class="container mt-5">
+                        <div class="row d-flex justify-content-center align-items-center gy-4">
+                        ${this.data.map((employee) => {
+                            return`
+                            <div class="col-1" style="width: 18rem;">
+                            <div class="card bg-dark text-dark">
+                                <div class="card-body bg-warning">
+                                    <h2>${employee.getName()}</h2>
+                                    <p class="h4"><span><i class="fas fa-user-tie"></i> </span>${employee.getRole()}</p>
+                                </div>
+                                <div class="card-body text-white">
+                                    <ul class="list-group list-group-flush my-4 ">
+                                        <li class="list-group-item bg-secondary">ID: ${employee.getID()}</li>
+                                        <li class="list-group-item bg-secondary">Email: <a href="mailto:${employee.getEmail()}"> ${employee.getEmail()} </a></li>
+                                        <li class="list-group-item bg-secondary"> ${employee.getOffice ? `Office: ${employee.getOffice()}`: employee.getGitHub ? `GitHub: ${employee.getGitHub()}` : `School: ${employee.getSchool()}` }</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        `
+                        })}
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </body>
+        
+        </html>
+        `
     }
 
 
