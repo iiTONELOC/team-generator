b01830702fa445d1e313e6fe1bb0c378dd4aedf8
iiTONELOC <anthonytropeano@me.com>
2021-02-22 00:04:08 -0500

    refactored pageGenerate functions

---
+++ utils/createPage.js
@@ -0,0 +1,26 @@
+const { writeFile, copyFile } = require('../utils/generateSite');
+const GeneratePage = require('../src/page-template')
+const createPage = (data) => {
+    
+                    let myPromise = new Promise(function (myResolve, myReject) {
+                        // "Producing Code" (May take some time)
+                        myResolve(new GeneratePage(data).createPage()); // when successful
+                        myReject("Error");  // when error
+                    });
+                    // "Consuming Code" (Must wait for a fulfilled Promise)
+                    myPromise.then(pageHTML => {
+                        return writeFile(pageHTML);
+                    })
+                        .then(writeFileResponse => {
+                            console.log(writeFileResponse);
+                            return copyFile();
+                        })
+                        .then(copyFileResponse => {
+                            console.log(copyFileResponse);
+                        })
+                        .catch(err => {
+                            console.log(err);
+                        });
+}
+
+module.exports=createPage
